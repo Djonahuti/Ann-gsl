753bf6f1cdcf068a67ad56daacc6ac18d5f2a7da
Djonahuti <djonah04@gmail.com>
2025-11-04 15:47:21 +0100

    commit

--- src/types/theme.ts
+++ src/types/theme.ts
@@ -1,15 +1,7 @@
-import type { Config } from 'tailwindcss'
-
 type WithOpacity<T> = T | ((params: { opacityValue: number | undefined }) => T)
 
 export type ThemeColors = {
   primary: WithOpacity<string>
   'primary-light': WithOpacity<string>
   'primary-dark': WithOpacity<string>
-}
-
-declare module 'tailwindcss' {
-  interface CorePlugins {
-    preflight: false
-  }
 }
