fb11592db0a3884bd02dc7784b3e1f730f4a1f05
Djonahuti <djonah04@gmail.com>
2025-11-05 15:03:06 +0100

    commit

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -280,7 +280,7 @@ export default function ContactCTA() {
           <path strokeLinecap="round" strokeLinejoin="round" d="M5 10l7-7m0 0l7 7m-7-7v18" />
         </svg>
       </button>
-    )}      
+    )}
     </section>
   );
 }
